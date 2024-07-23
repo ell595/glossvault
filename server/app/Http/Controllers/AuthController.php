96825f7e691a43e8a23a530a8a272e8b4217a6fd
Elliot <e.fraser3@gmail.com>
2024-07-24 00:42:02 +0100

    Updated register function

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
             // Validate data submitted via request
             $this->validate($request, [
                 'email' => ['required', 'email', 'unique:users'],
-                'username' => ['required', 'username', 'unique:users'],
+                'username' => ['required'],
                 'password' => 'required'
             ]);
 
